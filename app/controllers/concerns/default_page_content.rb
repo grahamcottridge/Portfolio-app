7a555f0f40ff1ff961dfbe8291a17874b72eb1e1
Graham Cottridge <codinggc@gmail.com>
2018-01-08 20:00:30 +0000

    Added nav bar and added default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,12 @@
+module DefaultPageContent
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_page_defaults
+  end
+
+  def set_page_defaults
+    @page_title = "Portfolio"
+    @seo_keywords = "Graham Cottridge portfolio"
+  end
+end
