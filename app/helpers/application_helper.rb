70a0457cc79886a765bd63f25aafaa26f420c187
Graham Cottridge <codinggc@gmail.com>
2018-01-10 00:43:18 +0000

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    ViewTool::Renderer.copyright 'Graham Cottridge', 'All rights reserved'
+  end
 end
