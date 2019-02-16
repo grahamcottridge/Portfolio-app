59adb54bc7e185f5964888327fc01b87ce2d7ab6
Graham Cottridge <codinggc@gmail.com>
2019-02-16 23:16:26 +0000

    Update about me

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
 //= require rails-ujs
+//= require bootstrap-sprockets
 //= require jquery-ui
 //= require html.sortable
 //= require cocoon
